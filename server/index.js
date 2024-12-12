a61ecba0360879ea284488bdf4c9cef0f177feb6
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-12-13 05:02:45 +0600

    Installation and Setup the Environments

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: "process.env.FRONTEND_URL"
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+}));
+
+const PORT = 8080|| process.env.PORT;
+
+app.get('/', (request, response) =>{
+    //server to client
+    response.json({
+        message: "Server to the backend API! " + PORT 
+    })
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server running on port", PORT);
+})
